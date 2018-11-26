11d1a4b482af5cbf719becaebe4f4e070919b4a1
Matthew Greene <mgreene@kaleidasystems.com>
2018-11-26 13:28:09 -0500

    Initial commit

---
+++ src/main/java/com/littlefoot/littlefoot/LittlefootApplication.java
@@ -0,0 +1,12 @@
+package com.littlefoot.littlefoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittlefootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittlefootApplication.class, args);
+	}
+}
