372a2508995ca3e8a555bf7684cfcf9beb4875ec
matthewgreene <mgreene57005@gmail.com>
2018-11-26 20:04:03 -0500

    Created security package

--- src/main/java/com/littlefoot/littlefoot/sercurity/SecurityConfig.java
+++ src/main/java/com/littlefoot/littlefoot/sercurity/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.littlefoot.littlefoot;
+package com.littlefoot.littlefoot.sercurity;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
