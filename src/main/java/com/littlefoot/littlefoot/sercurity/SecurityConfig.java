b73e44d858713f36ea4f982309fed65f58e12dab
matthewgreene <mgreene57005@gmail.com>
2018-11-26 20:28:21 -0500

    Ignoring csrf protectiong for h2-console (don't use in prod)

--- src/main/java/com/littlefoot/littlefoot/sercurity/SecurityConfig.java
+++ src/main/java/com/littlefoot/littlefoot/sercurity/SecurityConfig.java
@@ -1,10 +1,16 @@
 package com.littlefoot.littlefoot.sercurity;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/h2-console/**")
+                .and().headers().frameOptions().sameOrigin();
+    }
 }
