7c4e2f679bed2a73cdbf8673315060501cfb7c69
Matthew Greene <mgreene@kaleidasystems.com>
2018-12-11 08:42:21 -0500

    Creating user entity.

---
+++ src/main/java/com/littlefoot/littlefoot/User.java
@@ -0,0 +1,49 @@
+package com.littlefoot.littlefoot;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import java.util.Arrays;
+import java.util.Collection;
+
+@Entity
+@Data
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@AllArgsConstructor
+public class User implements UserDetails {
+    private final String username;
+    private final String password;
+    private final String firstName;
+    private final String lastName;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
