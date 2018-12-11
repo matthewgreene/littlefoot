56ab0789780a201b42986e21085090c768136815
Matthew Greene <mgreene@kaleidasystems.com>
2018-12-11 08:51:15 -0500

    Adding Id field.

--- src/main/java/com/littlefoot/littlefoot/entity/User.java
+++ src/main/java/com/littlefoot/littlefoot/entity/User.java
@@ -9,6 +9,9 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -17,6 +20,10 @@ import java.util.Collection;
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 @AllArgsConstructor
 public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private final String username;
     private final String password;
     private final String firstName;
