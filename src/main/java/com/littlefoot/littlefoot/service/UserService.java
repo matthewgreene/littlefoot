2bb0b46b7092f9fc852041bee218bf6a3bcdebd7
Matthew Greene <mgreene@kaleidasystems.com>
2018-12-11 08:55:31 -0500

    Adding UserService.

---
+++ src/main/java/com/littlefoot/littlefoot/service/UserService.java
@@ -0,0 +1,24 @@
+package com.littlefoot.littlefoot.service;
+
+import com.littlefoot.littlefoot.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found"));
+    }
+}
